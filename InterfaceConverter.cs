614e516dc679f0d9878128657cd0e67394733c53
Václav Pekárek <v.pekarek@gmail.com>
2021-12-26 19:27:40 +0100

    Add project files.

---
+++ InterfaceConverter.cs
@@ -0,0 +1,75 @@
+﻿using System.Reflection;
+using System.Text.Json.Serialization;
+
+namespace System.Text.Json;
+public class InterfaceConverter<T> : JsonConverter<T>
+    where T : class
+{
+    public override T? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        Utf8JsonReader readerClone = reader;
+        if (readerClone.TokenType != JsonTokenType.StartObject)
+        {
+            throw new JsonException();
+        }
+
+        readerClone.Read();
+        if (readerClone.TokenType != JsonTokenType.PropertyName)
+        {
+            throw new JsonException();
+        }
+
+        string propertyName = readerClone.GetString();
+        if (propertyName != "$type")
+        {
+            throw new JsonException();
+        }
+
+        readerClone.Read();
+        if (readerClone.TokenType != JsonTokenType.String)
+        {
+            throw new JsonException();
+        }
+
+        string typeValue = readerClone.GetString();
+        var instance = Activator.CreateInstance(Assembly.GetExecutingAssembly().FullName, typeValue).Unwrap();
+        var entityType = instance.GetType();
+
+        var deserialized = JsonSerializer.Deserialize(ref reader, entityType, options);
+        return (T)deserialized;
+    }
+
+    public override void Write(Utf8JsonWriter writer, T value, JsonSerializerOptions options)
+    {
+        switch (value)
+        {
+            case null:
+                JsonSerializer.Serialize(writer, (T)null, options);
+                break;
+            default:
+                {
+                    var type = value.GetType();
+                    using var jsonDocument = JsonDocument.Parse(JsonSerializer.Serialize(value, type, options));
+                    writer.WriteStartObject();
+                    writer.WriteString("$type", type.FullName);
+
+                    foreach (var element in jsonDocument.RootElement.EnumerateObject())
+                    {
+                        element.WriteTo(writer);
+                    }
+
+                    writer.WriteEndObject();
+                    break;
+                }
+        }
+    }
+}
+
+[AttributeUsage(AttributeTargets.Interface, AllowMultiple = false)]
+public class JsonInterfaceConverterAttribute : JsonConverterAttribute
+{
+    public JsonInterfaceConverterAttribute(Type converterType)
+        : base(converterType)
+    {
+    }
+}
---
+++ readme.md
@@ -0,0 +1,21 @@
+﻿# Interface converter for System.Text.Json
+
+## Description
+Use this converter when you need to serialize and deserialize interface types, or list of object with single interface.
+
+Deserialized entity has the original type as serialized.
+
+## Usage
+
+Specify `JsonInterfaceConverter` on any interface you want to serialize using this converter.
+
+```C#
+[JsonInterfaceConverter(typeof(InterfaceConverter<IExample>))]
+public interface IExample
+{
+    int Id { get; set; }
+    string Name { get; set; }
+}
+
+```
+
